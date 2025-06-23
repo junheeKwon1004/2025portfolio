bc2f3d659b4a05cbbb567a92052cf874170dd029
kwonjunhee <htt1247@naver.com>
2025-06-23 11:00:29 +0900

    📁 Naver 클론코딩 위치 이동 및 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }: { mode: string }) => ({
+  base: mode === 'production' ? '/2025portfolio/' : '/',
   plugins: [react()],
-  base: '/2025portfolio',
-})
+}));
+
