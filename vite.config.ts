7a1872e2680d353c4c17c29cd092499e182f01ed
kwonjunhee <htt1247@naver.com>
2025-06-23 12:10:48 +0900

    ✅ fix: base 경로 깃허브 주소에 맞게 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2025portfolio',
+  base: '/2025ortfolio',
   build: {
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, 'index.html'),
-      naver: resolve(__dirname, 'projects/naver/index.html'), // ✅
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        naver: resolve(__dirname, 'projects/naver/index.html'),
+      },
     },
   },
-}
 })
